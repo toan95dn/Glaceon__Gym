86ee129d64e95835ffe84af20fa8304015e64fb8
toan95dn <51420692+toan95dn@users.noreply.github.com>
2021-11-05 21:00:29 -0700

    add mongoDB connection

--- server/app.js
+++ server/app.js
@@ -1,15 +1,22 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT;
 
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(process.env.PORT);
+    console.log("Connect to MongoDB successfully");
+  })
+  .catch((err) => console.log(err));
+
 app.get("/", (req, res) => {
   res.send("Hello Glaceon Team!");
-});
-
-app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
-  console.log(process.env.PORT);
 });
