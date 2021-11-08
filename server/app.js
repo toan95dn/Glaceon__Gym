329c514e585cf27b05ceadd46717c65f1e09e60d
toan95dn <51420692+toan95dn@users.noreply.github.com>
2021-11-08 14:26:36 -0800

    finish auth, still need to add mail verification

--- server/app.js
+++ server/app.js
@@ -1,11 +1,30 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import passport from "passport";
+import LocalStrategy from "passport-local";
+import User from "./models/User.mjs";
+import authRoutes from "./routes/auth.mjs";
+import morgan from "morgan";
+import session from "express-session";
 
 dotenv.config();
 const app = express();
-const port = process.env.PORT;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+app.use(
+  session({ secret: "not a good one", resave: true, saveUninitialized: true })
+);
 
+//Config Passport
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//Connect to MongoDB
 mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
@@ -19,4 +38,15 @@ mongoose
 
 app.get("/", (req, res) => {
   res.send("Hello Glaceon Team!");
+});
+
+app.use("/auth", authRoutes);
+
+//Error handling
+app.use(function (err, req, res, next) {
+  console.log("HIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII");
+  console.error("Message from backend: ", err.message);
+  res
+    .status(err.statusCode ? err.statusCode : 500)
+    .json({ message: err.message });
 });
---
+++ server/routes/users.mjs
@@ -0,0 +1,1 @@
+//This file is used to update user infomation
