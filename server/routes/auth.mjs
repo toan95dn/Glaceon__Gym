4dd4f1f385f082e904a7af6c4208667331418888
toan95dn <51420692+toan95dn@users.noreply.github.com>
2021-11-08 14:25:37 -0800

    add auth routes

---
+++ server/routes/auth.mjs
@@ -0,0 +1,37 @@
+//This file is used for authentication (signup/login)
+import express from "express";
+import passport from "passport";
+import User from "../models/User.mjs";
+import { validateSignup, validateSignin } from "../middleware.mjs";
+import createUser from "../controllers/createUser.mjs";
+import catchAsync from "../utils/catchAsync.mjs";
+
+const authRoutes = express.Router();
+
+//Sign up
+authRoutes.post("/signup", validateSignup, catchAsync(createUser));
+
+//Sign in
+authRoutes.post(
+  "/signin",
+  passport.authenticate("local", {
+    failureRedirect: "/errorlogin",
+    successRedirect: "/suclogin",
+  })
+);
+
+//Sign out
+authRoutes.get("/signout", (req, res) => {
+  console.log("logging out ...");
+  console.log(req.user._id);
+  req.logout();
+  res.send("out");
+});
+
+authRoutes.get("/islogin", (req, res) => {
+  console.log("CHeck is login");
+  console.log(req.isAuthenticated());
+  res.send(req.isAuthenticated());
+});
+
+export default authRoutes;
