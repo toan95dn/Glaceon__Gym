832420896cc857d6a95d82358f31f62c443e78f2
toan95dn <51420692+toan95dn@users.noreply.github.com>
2021-11-13 19:59:27 -0800

    update user schema

--- server/models/User.mjs
+++ server/models/User.mjs
@@ -1,11 +1,19 @@
 import mongoose from "mongoose";
 import passportLocalMongoose from "passport-local-mongoose";
+import { boolean } from "yup";
 
 const UserSchema = new mongoose.Schema(
   {
     email: {
       type: String,
       required: true,
+      unique: true,
+    },
+    active: {
+      type: boolean,
+    },
+    activateCode: {
+      type: String,
       unique: true,
     },
   },
